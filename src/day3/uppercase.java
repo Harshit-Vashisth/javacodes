6d5d54dce6a3c42b4d43fdcacbbadc33e9c0f66d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-07 18:21:21 +0530

    Update uppercase.java

--- src/day3/uppercase.java
+++ src/day3/uppercase.java
@@ -14,13 +14,15 @@ public class uppercase {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         ArrayList<String> arr=new ArrayList<String>();
-        System.out.println("Enter the number String");
-        int num=sc.nextInt();
-        System.out.println("Enter the strings");
-        for(int i=0;i<num;i++) {
-            String n = sc.nextLine();
-            arr.add(n);
-        }
+
+        arr.add("haasas");
+        arr.add("Djsaid");
+        arr.add("jands");
+        arr.add("haasas");
+        arr.add("Djsaid");
+        arr.add("jands");
+
+        int num=arr.size();
         System.out.println(arr);
         upper(arr,num-1);
         System.out.println(arr);
