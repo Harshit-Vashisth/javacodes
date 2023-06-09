413f58c5aa2575a497c5a9c142f3b94b304fe4fe
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-10 04:02:58 +0530

    Update patternsquare.java

--- src/day4/patternsquare.java
+++ src/day4/patternsquare.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class patternsquare {
     static void solve(int i,int n){
-        if(i==n)
+        if(i>=n)
             return;
         for(int j=0;j<n;j++)
-            System.out.println(" * ");
+            System.out.print(" * ");
+        System.out.println();
         solve(i+1,n);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the value of n");
         int n=sc.nextInt();
-        for(int i=0;i<n;i++)
         solve(0,n);
     }
 }
