13f1734a1f3c11e288a08d1bc7e73ffe0446508d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-10 14:20:30 +0530

    Update pyramidpattern.java

--- src/day4/pyramidpattern.java
+++ src/day4/pyramidpattern.java
@@ -1,4 +1,26 @@
 package day4;
 
+import java.util.Scanner;
+
 public class pyramidpattern {
+    static void print(int i , int n,int k){
+        if(i>n)
+            return;
+        for (int j = 1; j <= n; j++) {
+
+            // Condition to print star pattern
+            if (j >= k)
+                System.out.print("* ");
+            else
+                System.out.print("  ");
+        }
+        System.out.println();
+        print(  i+1,n,k-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no of elements");
+        int n=sc.nextInt();
+        print(1,n,n);
+    }
 }
