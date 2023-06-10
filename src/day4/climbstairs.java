03fdab8740ad068418ea5319f5dc17cdfb627772
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-10 15:40:56 +0530

    Create climbstairs.java

---
+++ src/day4/climbstairs.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class climbstairs {
+
+    class Solution {
+        public:
+        public int climbStairs(int n) {
+            if(n==1||n==2)
+                return n;
+            return climbStairs(n-1)+climbStairs(n-2);
+        }
+    };
+}
